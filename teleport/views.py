85d9b8f32c7a68f9819d4387d3051f533cabb0c2
Anant Bhardwaj <anantb@csail.mit.edu>
2013-05-07 19:41:31 -0400

    add new contact

--- teleport/views.py
+++ teleport/views.py
@@ -160,7 +160,7 @@ def add_contact(request):
         user2 = request.POST["email"]
         c = Contact(user1=user1, user2=user2)
         c.save()
-        return HttpResponse('/contacts.html', {'invite':user2})
+        return HttpResponse(json.dumps({'invite':user2}), mimetype="application/json")
     else:
         return render_to_response('add_contact.html')
 
