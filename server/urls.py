007c1a22c363bd7dce74887482b39edec34f7298
Anant Bhardwaj <anantb@csail.mit.edu>
2013-05-07 17:37:48 -0400

    more things

--- server/urls.py
+++ server/urls.py
@@ -22,5 +22,7 @@ urlpatterns = patterns('',
 
     url(r'^login', 'teleport.views.login'),
     url(r'^register', 'teleport.views.register'),
+    url(r'^settings', 'teleport.views.settings'),
+    url(r'^invite_email/(\w+)$', 'server.views.invite_email'),
     url(r'^logout', 'teleport.views.logout'),
 )
